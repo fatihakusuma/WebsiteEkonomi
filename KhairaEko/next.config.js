2d6c8992c7144dba0e3bf5fe06a531a885646905
fatihakusuma <fatihakusumawardhani@gmail.com>
2025-07-20 18:26:55 +0700

    Update next.config.js

--- KhairaEko/next.config.js
+++ KhairaEko/next.config.js
@@ -1,6 +1,21 @@
+// next.config.js
 module.exports = {
   reactStrictMode: true,
   experimental: {
     esmExternals: false,
+    // Tambahkan ini untuk memory management
+    workerThreads: true,
+    cpus: 2
   },
+  webpack: (config, { isServer }) => {
+    // Tambahkan handling untuk PDF.js
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    
+    // Fix memory issue
+    config.parallelism = 2;
+    config.cache = false;
+    
+    return config;
+  }
 };
